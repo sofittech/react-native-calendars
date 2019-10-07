26f20509dddd9582feeb60ed1983f12871c809eb
Aqib <aqibbangash@sofittech.com>
2019-10-07 15:26:50 +0500

    Custom

---
+++ src/calendar/style.js
@@ -0,0 +1,31 @@
+import {StyleSheet} from 'react-native';
+import * as defaultStyle from '../style';
+
+const STYLESHEET_ID = 'stylesheet.calendar.main';
+
+export default function getStyle(theme={}) {
+  const appStyle = {...defaultStyle, ...theme};
+  return StyleSheet.create({
+    container: {
+      paddingLeft: 5,
+      paddingRight: 5,
+      backgroundColor: appStyle.calendarBackground
+    },
+    monthView: {
+      backgroundColor: appStyle.calendarBackground
+    },
+    week: {
+      marginTop: 7,
+      marginBottom: 7,
+      justifyContent: 'space-around',
+      flexDirection: 'row'
+    },
+    weekAR: {
+      marginTop: 7,
+      marginBottom: 7,
+      justifyContent: 'space-around',
+      flexDirection: 'row-reverse'
+    },
+    ...(theme[STYLESHEET_ID] || {})
+  });
+}
